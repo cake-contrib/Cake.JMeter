66ab8b1518d6ab98932bf767c972f983ce45e79b
Roman Bäriswyl <roman.baeriswyl@elca.ch>
2019-02-08 16:28:08 +0100

    Updated to .net standard, updated cake

---
+++ src/Cake.JMeter/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyInformationalVersion("1.1.0.0-feature/UpdateCakeAndNetStandard")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
+
+[assembly: InternalsVisibleTo("Cake.JMeter.Tests")]
